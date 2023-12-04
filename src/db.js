f255bdd0269d447c81cfec1e1dc916423f60eb4a
phamty <114122026+Phamty2002@users.noreply.github.com>
2023-12-04 11:38:13 +0700

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,19 +2,19 @@
 
 const mysql = require('mysql');
 
- const db = mysql.createConnection({
+ /*const db = mysql.createConnection({
   host: 'db4free.net',
   user: 'robertoty',
   password: 'Robertoty2002',
   database: 'foodweb',
-}); 
+}); */
 
-/*const db = mysql.createConnection({
+const db = mysql.createConnection({
   host: 'food-database.cvpmqafzaqqx.us-east-1.rds.amazonaws.com',
   user: 'admin',
   password: 'Robertoty2002',
   port     : 3306
-}); */
+}); 
 
 
 db.connect(err => {
